8d3d6ab162055482c072ac0fde2f5851e1afa2de
Eric Bellm <ecbellm@uw.edu>
2024-09-20 16:12:18 -0700

    avoid circular import

--- src/tasso/schema/label.py
+++ src/tasso/schema/label.py
@@ -1,10 +1,18 @@
 """The label database table."""
 
+# https://stackoverflow.com/questions/75919378/how-to-handle-circular-imports-in-sqlalchemy
+from typing import TYPE_CHECKING
+
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import Base
-from .classification import Classification
-from .run_label import RunLabel
+
+if TYPE_CHECKING:
+    from .classification import Classification
+    from .run_label import RunLabel
+else:
+    Classification = "Classification"
+    RunLabel = "RunLabel"
 
 __all__ = ["Label"]
 
@@ -16,6 +24,6 @@ class Label(Base):
 
     label_id: Mapped[int] = mapped_column(primary_key=True)
     label: Mapped[str]
-    classifications: Mapped[list["Classification"]] = relationship(
+    classifications: Mapped[list[Classification]] = relationship(
         secondary=RunLabel, back_populates="labels"
     )
