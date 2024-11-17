7789475a6f8b794403288ea7b31090305fed15d9
Eric Bellm <ecbellm@uw.edu>
2024-11-16 20:25:16 -0700

    add missing relationship

--- src/tasso/schema/classification_run.py
+++ src/tasso/schema/classification_run.py
@@ -3,7 +3,7 @@
 from datetime import datetime
 
 from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import Base
 
@@ -24,3 +24,5 @@ class ClassificationRun(Base):
     ticket: Mapped[str | None]
     time_start: Mapped[datetime | None]
     time_stop: Mapped[datetime | None]
+
+    subjects: Mapped[list["Subject"]] = relationship()  # noqa: F821
